c78498ba6ac4367718e37cee80aa128e6f573a45
ren.kataoka <ren.kataoka@optim.co.jp>
2020-12-12 14:09:35 +0900

    プロジェクトを作成。

---
+++ app/src/main/java/jp/co/optim/techblog_android_room_sample/MainActivity.java
@@ -0,0 +1,14 @@
+package jp.co.optim.techblog_android_room_sample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
