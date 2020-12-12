d72980b9827ced8a9dd069d6bb920351ec493240
ren.kataoka <ren.kataoka@optim.co.jp>
2020-12-12 15:25:33 +0900

    メイン画面にToolbarを追加し、削除メニューを実装。

--- app/src/main/java/jp/co/optim/techblog_android_room_sample/MainActivity.java
+++ app/src/main/java/jp/co/optim/techblog_android_room_sample/MainActivity.java
@@ -1,8 +1,12 @@
 package jp.co.optim.techblog_android_room_sample;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +15,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //toolbarを設定
+        Toolbar toolbar = findViewById(R.id.toolbarMain);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_delete_all:
+                //AllDelete処理
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 
     public void onClickAddUserButton(View view) {
